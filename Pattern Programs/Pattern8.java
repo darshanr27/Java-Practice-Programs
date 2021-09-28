5816861a12cafd8e8636f0b191ba3923b05cc4ef
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-09-28 19:36:59 +0530

    Create Pattern8.java

---
+++ Pattern Programs/Pattern8.java
@@ -0,0 +1,34 @@
+/*
+
+Pattern 8
+
+     *
+    * *
+   *   *
+  *     *
+ *********
+
+*/
+
+
+public class Pattern8 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int k = 1; k <= 6-i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= (2*i)-1; j++) {
+				if(j==1 || j==(2*i)-1 || i==5) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
