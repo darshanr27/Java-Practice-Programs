42124c446f0eeadace643a1e8491a29b54dece1f
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-09-28 19:39:11 +0530

    Create Pattern9.java

---
+++ Pattern Programs/Pattern9.java
@@ -0,0 +1,31 @@
+/*
+
+Pattern 9
+
+ *********
+  *     *
+   *   *
+    * *
+     *
+
+*/
+
+public class Pattern9 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int k = 1; k <= i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 11-(2*i); j++) {
+				if(i==1 || i==5 || j==1 || j == 11-(2*i))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
