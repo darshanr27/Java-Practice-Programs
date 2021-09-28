29d11a3be75eb3751d33dbe288088bba1297bf80
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-09-28 19:31:58 +0530

    Create Pattern2.java

---
+++ Pattern Programs/Pattern2.java
@@ -0,0 +1,25 @@
+/*
+
+Pattern 2
+
+*****
+****
+***
+**
+*
+
+*/
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 6-i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
