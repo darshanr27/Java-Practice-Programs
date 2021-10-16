6e1f403a4ab0865eecf1c50fb199b87338a90411
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-10-16 12:54:09 +0530

    Create Pattern4.java

---
+++ Pattern Programs/Pattern4.java
@@ -0,0 +1,27 @@
+/*
+
+Pattern 4
+
+     *****
+     *****
+     *****
+     *****
+     *****
+
+*/
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for(int k = 1; k <= 5; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 5; j++) {
+			System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+}
