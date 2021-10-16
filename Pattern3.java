8821e1925f5fdbdde1a74932bea06f09ee4f5c52
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-10-16 12:46:23 +0530

    Create Pattern3.java

---
+++ Pattern3.java
@@ -0,0 +1,27 @@
+/*
+
+Pattern 3
+
+*****
+*****
+*****
+*****
+*****
+
+*/
+
+package com.programmingsession.patternPrograms;
+
+public class Pattern3 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
