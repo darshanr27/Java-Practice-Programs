1e4a938d23207d7135124fca47dd9b2aa6aded02
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-10-16 13:37:00 +0530

    Update Pattern3.java

--- Pattern Programs/Pattern3.java
+++ Pattern Programs/Pattern3.java
@@ -10,9 +10,7 @@ Pattern 3
 
 */
 
-
 public class Pattern3 {
-
 	public static void main(String[] args) {
 		for (int i = 1; i <= 5; i++) {
 			for (int j = 1; j <= 5; j++) {
@@ -20,7 +18,5 @@ public class Pattern3 {
 			}
 			System.out.println();
 		}
-
 	}
-
 }
