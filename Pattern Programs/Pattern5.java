8b4aeeb6caa68db4d974c8d290bf76a09a70efa2
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-10-16 13:22:03 +0530

    Create Pattern5.java

---
+++ Pattern Programs/Pattern5.java
@@ -0,0 +1,25 @@
+/*
+
+Pattern 5
+
+     *
+    ***
+   *****
+  *******
+ *********
+
+*/
+
+public class Pattern5 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 5; i++) {
+			for(int k = 1; k <= 6 - i; k++) {
+				System.out.print(" ");
+			}
+			for(int j = 1; j <= (2*i)-1; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
