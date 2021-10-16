95568576bd588997ac0f6065c29c2ecb46e37f77
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-10-16 15:35:03 +0530

    Create Pattern10.java

---
+++ Pattern Programs/Pattern10.java
@@ -0,0 +1,26 @@
+/*
+
+Pattern 10
+
+1
+12
+123
+1234
+12345
+
+*/
+
+
+public class Pattern10 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
