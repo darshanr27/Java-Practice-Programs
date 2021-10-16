95b772fe15c4a94170b86171fc12bed4a07bf197
Darshan R <41225547+darshanr27@users.noreply.github.com>
2021-10-16 13:35:27 +0530

    Create Pattern6.java

---
+++ Pattern Programs/Pattern6.java
@@ -0,0 +1,26 @@
+/*
+
+Pattern 6
+
+ *********
+  *******
+   *****
+    ***
+     *
+
+*/
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for(int k = 1; k <= i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= 11-(2*i); j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
